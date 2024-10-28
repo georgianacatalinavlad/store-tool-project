0312575de9bf9cbb414b7a51597d7c09ccc57e30
georgiana <georgianavlad16@yahoo.ro>
2024-10-28 12:27:12 +0200

    Initial commit

---
+++ src/main/java/org/example/storetoolproject/StoreToolProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.storetoolproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreToolProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreToolProjectApplication.class, args);
+    }
+
+}
